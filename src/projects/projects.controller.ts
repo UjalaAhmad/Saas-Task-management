18e54dba9ec8b38e16c49c66b45a92cdd2b9e7ae
UjalaAhmad <ujala.muzaffar@gmail.com>
2026-02-07 00:37:47 +0500

    Fix build for Render (remove nest CLI dependency)

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,9 +1,9 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guards';
-import { RolesGuard } from 'src/common/gurads/roles.guards';
+import { JwtAuthGuard } from '../auth/jwt-auth.guards';
+import { RolesGuard } from '../common/gurads/roles.guards';
 import { Roles } from '../common/decorators/roles.decorator';
-import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { CurrentUser } from '../common/decorators/current-user.decorator';
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('projects')
 export class ProjectsController {
