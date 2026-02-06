0f7c80a041d64853d8e6f6e66c87255ed87ec956
UjalaAhmad <ujala.muzaffar@gmail.com>
2026-02-07 00:13:30 +0500

    update

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'http://localhost:3000',
-    ],
-    credentials: true,
-  });
+  origin: [
+    'http://localhost:3001',
+    'https://YOUR-VERCEL-APP.vercel.app',
+  ],
+  credentials: true,
+});
+
 
   await app.listen(3000);
 }
